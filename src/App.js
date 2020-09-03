c8eaa9efd88fe54c6c51f1bed18564088208898a
gloriango <gloriango12@gmail.com>
2020-09-03 22:34:25 +1000

    cleaned up app spacing

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 import Signup from "./Components/Signup/Signup";
 import Login from "./Components/Login/Login";
 import ForgotPassword from "./Components/ForgotPassword/ForgotPassword";
 
 import "./App.css";
 
-class App extends Component {
-  render() {
+const App = () =>{
     return (
-      <div className="App">
-        <h1> LOG IN STUFF</h1>
+      <div className="App">        
         {/* later on would have to split them into different html pages each */}
+        <h1> LOG IN STUFF</h1>
         <Login />
+
         <h1> SIGN UP STUFF</h1>
         <Signup />
 
-       
-
         <h1> FORGOT PASSWORD STUFF</h1>
-
         <ForgotPassword/>
 
       </div>
     );
-  }
 }
 
 export default App;
