28f059a6da1a458a344a8fbfd7785eb26e6b1955
ryfprivate <ryfprivate@outlook.com>
2020-09-04 12:09:03 +1000

    Added index to each folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from "react";
-import Signup from "./Components/Signup/Signup";
-import Login from "./Components/Login/Login";
-import ForgotPassword from "./Components/ForgotPassword/ForgotPassword";
+import Signup from "./Components/Signup";
+import Login from "./Components/Login";
+import ForgotPassword from "./Components/ForgotPassword";
 
 import "./App.css";
 
-const App = () =>{
-    return (
-      <div className="App">        
-        {/* later on would have to split them into different html pages each */}
-        <h1> LOG IN STUFF</h1>
-        <Login />
+const App = () => {
+  return (
+    <div className="App">
+      {/* later on would have to split them into different html pages each */}
+      <h1> LOG IN STUFF</h1>
+      <Login />
 
-        <h1> SIGN UP STUFF</h1>
-        <Signup />
+      <h1> SIGN UP STUFF</h1>
+      <Signup />
 
-        <h1> FORGOT PASSWORD STUFF</h1>
-        <ForgotPassword/>
+      <h1> FORGOT PASSWORD STUFF</h1>
+      <ForgotPassword />
 
-      </div>
-    );
+    </div>
+  );
 }
 
 export default App;
