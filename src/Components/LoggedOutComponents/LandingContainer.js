5b45e04fc351329d6e8146f4b3523daf6f1de8f4
gloriango <gloriango12@gmail.com>
2020-09-27 20:28:37 +1000

    added loading component

--- src/Components/LoggedOutComponents/LandingContainer.js
+++ src/Components/LoggedOutComponents/LandingContainer.js
@@ -4,7 +4,6 @@ import { CssBaseline, Container, Paper } from '@material-ui/core'
 import { loggedOutStyles } from '../../Styles/loggedOutStyles'
 import DarkAndLightModeButton from '../CommonComponents/DarkAndLightModeButton'
 
-
 export default function LandingContainer(props) {
   const classes = loggedOutStyles()
   // Placeholder variable for the component to be rendered (i.e. Login, Signup,
@@ -14,7 +13,6 @@ export default function LandingContainer(props) {
   return (
     <Container component='main' className={classes.root}>
       <DarkAndLightModeButton />
-
       <CssBaseline />
       <Paper className={classes.paper}>{content}</Paper>
     </Container>
