39a22837149fbde07584c279ead1e8f1ab842277
Carl Cockerall <cockerall.carl.a@gmail.com>
2020-10-08 20:54:38 +1100

    Remade the Failed to find portfolio page
    - Added images folder to src with images used in the website
    - Downloaded and added the image from the landing page so we are not dependent on other sites

--- src/Components/LoggedOutComponents/PublicPortfolioFailed.js
+++ src/Components/LoggedOutComponents/PublicPortfolioFailed.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
 // import { BrowserRouter, Route, Switch, Redirect, Link } from 'react-router-dom'
 import LandingContainer from './LandingContainer'
-import {Typography, makeStyles} from '@material-ui/core'
+import {Typography, makeStyles, Grid} from '@material-ui/core'
+import clsx from 'clsx'
+import { ThemesContext } from '../Contexts/ThemesContext'
 
 const useStyles = makeStyles((theme) => ({
     content: {
@@ -10,18 +12,35 @@ const useStyles = makeStyles((theme) => ({
         width: '100%',
         alignItems: 'center',
         justifyContent: 'center',
+        paddingTop: '10%',
         //backgroundColor: theme.palette.background.default,
         padding: theme.spacing(3)
+    },
+    image: {
+        maxWidth: '60%',
+        height: 'auto'
+    },
+    lightImage: {
+        filter:
+          'invert(99%) sepia(4%) saturate(239%) hue-rotate(249deg) brightness(117%) contrast(100%)'
     }
   }))
 
 export default function PublicPortfolioFailed(props) {
   const classes = useStyles()
+  const { currentTheme: theme } = useContext(ThemesContext)
 
   const content = (
     <div className={classes.content}>
-      <Typography align='center' variant='h3' >Sorry! We could not find the specified portfolio</Typography>
-      <Typography align='center' variant='h5' >Please make sure you have the correct portfolio link before trying again</Typography>
+        <Typography align='center' variant='h4' >Sorry! We could not find the specified portfolio</Typography>
+        <Typography align='center' variant='h6' >Please make sure you have the correct portfolio link before trying again</Typography>
+        <center>
+        <img
+            className={theme === 'dark' ? clsx(classes.image, classes.lightImage) : classes.image}
+            src={require('../../Images/404.png')}
+            alt='not found'
+        />
+        </center>
     </div>
   )
 
